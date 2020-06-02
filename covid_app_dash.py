e2480d78907ebe2d4e07b6bc0790e7c7e2298be0
Ethan <ethan.king@rutgers.edu>
2020-06-01 20:59:26 -0400

    added deaths to plots, matched color to county cases

--- covid_app_dash.py
+++ covid_app_dash.py
@@ -16,8 +16,13 @@ pd.set_option('display.width', 1000)
 
 app = dash.Dash()
 
+# COLOR LIST - use this color list to force county cases + death color to match
+COLOR_LIST = ['rgb(127, 60, 141)', 'rgb(17, 165, 121)', 'rgb(57, 105, 172)', 'rgb(242, 183, 1)', 'rgb(231, 63, 116)', 'rgb(128, 186, 90)', 'rgb(230, 131, 16)', 'rgb(0, 134, 149)', 'rgb(207, 28, 144)', 'rgb(249, 123, 114)', 'rgb(165, 170, 153)']
+
 # read data
-covid = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv', parse_dates=[0])
+dTypes = {'county': str, 'state': str, 'fips': str, 'cases': int, 'deaths': int}
+covid = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv', dtype= dTypes, parse_dates=[0])
+# covid = covid.sort_values(by=['date', 'fips'], ascending=True)
 
 # options for state / county selection TODO: add options for major U.S. cities in multi select dropdown
 stateCounty = covid.groupby(['state', 'county']).min().reset_index().drop(columns=['fips', 'cases', 'deaths']).dropna()
@@ -29,7 +34,7 @@ for index, row in stateCounty.iterrows():
 	myDict['value'] = row['county'] + ',' + row['state']
 	options.append(myDict)
 
-# LAYOUT # TODO: Incorporate death numbers in plots
+# LAYOUT
 app.layout = html.Div([
 	html.H1('U.S. Counties Covid-19 Tracker'),
 	html.Div(
@@ -74,7 +79,10 @@ app.layout = html.Div([
 	),
 ])
 
-# update
+# TODO: create a density map of covid cases to use for timeseries input selection for counties
+
+
+# update dashboard
 @app.callback(
 	Output('my_graph','figure'),
 	[Input('submit-button','n_clicks')],
@@ -88,18 +96,21 @@ def update_graph(n_clicks, state_county, start_date, end_date):
 	start = datetime.strptime(start_date[:10], '%Y-%m-%d')
 	end = datetime.strptime(end_date[:10], '%Y-%m-%d')
 
-	#create traces for each ticker in option list
+	#create traces for each item in state_county
 	#TODO: add a trendline for the past two weeks
 	traces = []
 	titleNames= []
+	colorTracker = 0
 	for item in state_county:
 		countyStateList = item.split(',')
 		df = covid[(covid['date'] >= start) & (covid['date']<= end)]
 		df = df[(df['state'] == countyStateList[1]) & (df['county'] == countyStateList[0])]
-		print(df.head())
+		# print(df.head())
 		name = countyStateList[0] + ' County - ' + countyStateList[1]
-		traces.append({'x':df['date'], 'y':df['cases'], 'name': name})
+		traces.append({'x':df['date'], 'y':df['cases'], 'name': name + ' Cases', 'line': dict(color=COLOR_LIST[colorTracker % 11])})
+		traces.append({'x':df['date'], 'y':df['deaths'], 'name': name + ' Deaths', 'line': dict(color=COLOR_LIST[colorTracker % 11], dash='dash')})
 		titleNames.append(name)
+		colorTracker += 1
 
 	fig = {
 		'data': traces,
